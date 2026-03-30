14a0ec453cf889ceb86ce2752539f9f328531462
Ayushman <ayushman1502singh@gmail.com>
2026-03-30 07:52:10 +0530

    Added user input and multiple student storage using ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,59 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main{
     public static void main(String[] args) {
 
-        Student s1 = new Student();
-        s1.id = 1;
-        s1.name = "Ayush";
-        s1.marks = 88.5;
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Student> students = new ArrayList<>();
 
-        s1.display();
+        while (true) {
+            System.out.println("\n--- Student Management System ---");
+            System.out.println("1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Exit");
+            System.out.print("Enter choice: ");
+
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    Student s = new Student();
+
+                    System.out.print("Enter ID: ");
+                    s.id = sc.nextInt();
+
+                    sc.nextLine(); // clear buffer
+
+                    System.out.print("Enter Name: ");
+                    s.name = sc.nextLine();
+
+                    System.out.print("Enter Marks: ");
+                    s.marks = sc.nextDouble();
+
+                    students.add(s);
+
+                    System.out.println("Student added successfully!");
+                    break;
+
+                case 2:
+                    if (students.isEmpty()) {
+                        System.out.println("No students found.");
+                    } else {
+                        for (Student st : students) {
+                            st.display();
+                            System.out.println("---------------");
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Exiting...");
+                    return;
+
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        }
     }
 }
